7b17c526073f0b8cb01691a700f4eb17ecd7fd7b
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-06-21 22:45:09 +0200

    added MyLogger.java

---
+++ src/main/java/sample/Utils/MyLogger.java
@@ -0,0 +1,53 @@
+package sample.Utils;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.logging.*;
+
+public class MyLogger {
+    
+    public static MyLogger instance = new MyLogger();
+    
+    private static Logger logger;
+    private static FileHandler fileHandler;
+    
+    public static MyLogger getInstance(){
+        return instance;
+    }
+    private MyLogger() {
+        SimpleFormatter simpleFormatter = new SimpleFormatter(){
+            @Override
+            public synchronized String format(LogRecord record) {
+                return LocalDateTime.now().toString() + " " +
+                               record.getLevel() + ":" +
+                               record.getLoggerName() + " " +
+                               record.getMessage() +"\n";
+            }
+        };
+        
+        logger = Logger.getLogger(MyLogger.class.getName());
+        logger.setLevel(Level.CONFIG);
+        logger.setUseParentHandlers(false);
+    
+        addConsoleHandler();
+        addFileHandler();
+        setOrChangeFormatter(simpleFormatter);
+    }
+    
+    private void addConsoleHandler(){
+        Handler handler = new ConsoleHandler();
+        handler.setLevel(Level.SEVERE);
+        logger.addHandler(handler);
+    }
+    private void addFileHandler(){
+        try {
+            fileHandler = new FileHandler("Charts_%u.log",true);
+        } catch (IOException e) {
+            System.exit(1);
+        }
+        logger.addHandler(fileHandler);
+    }
+    public void setOrChangeFormatter(SimpleFormatter newSimpleFormatter){
+        fileHandler.setFormatter(newSimpleFormatter);
+    }
+}
