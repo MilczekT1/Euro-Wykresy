60e120d5d7f5c1b2871dcae4ef28e9ed7d1132f0
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-06-21 23:43:37 +0200

    import sample.General.Configurator

--- src/main/java/sample/Main/Main.java
+++ src/main/java/sample/Main/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.General.Configurator;
 
 public final class Main extends Application {
 
