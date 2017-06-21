bfec01ed0425f868fbb5e28d1ceb3838e922197a
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-06-21 23:36:28 +0200

    Simple refactoring and added logging in exceptions

--- src/main/java/sample/Utils/Configurator.java
+++ src/main/java/sample/Utils/Configurator.java
@@ -1,4 +1,4 @@
-package sample.Main;
+package sample.Utils;
 
 import javafx.scene.control.TextInputDialog;
 import lombok.Cleanup;
@@ -6,9 +6,12 @@ import lombok.Cleanup;
 import java.io.*;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public final class Configurator {
     
+    private static Logger logger = MyLogger.getLogger();
     private static Configurator instance = new Configurator();
     private static Properties currentSettings;
     private static Properties defaultSettings;
@@ -22,6 +25,7 @@ public final class Configurator {
         File propertiesFile = new File("program.properties");
         if (!propertiesFile.exists()) {
             setDefaultSettings();
+            logger.log(Level.CONFIG, "Wprowadzono nowa konfiguracje");
         } else {
             importDefaultSettings();
         }
@@ -30,39 +34,42 @@ public final class Configurator {
         String adress = null;
         String user = null;
         String password = null;
+        Optional<String> result;
     
         TextInputDialog adressDialog = new TextInputDialog("Konfiguracja polaczenia");
         adressDialog.setTitle("Konfiguracja polaczenia");
         adressDialog.setHeaderText("Krok 1/3: IP + port");
-        
-        TextInputDialog userDialog = new TextInputDialog("Konfiguracja polaczenia");
-        userDialog.setTitle("Konfiguracja polaczenia");
-        userDialog.setHeaderText("Krok 2/3: Nazwa użytkownika");
-        
-        TextInputDialog passwordDialog = new TextInputDialog("Konfiguracja polaczenia");
-        passwordDialog.setTitle("Konfiguracja polaczenia");
-        passwordDialog.setHeaderText("Krok 3/3: Haslo");
-    
-        Optional<String> result = adressDialog.showAndWait();
+        result = adressDialog.showAndWait();
         if (result.isPresent()) {
             adress = result.get();
         }
         else {
-            System.exit(0);
+            logger.log(Level.CONFIG, "Nie wprowadzono adresu ip i/lub portu");
+            System.exit(1);
         }
+        
+        TextInputDialog userDialog = new TextInputDialog("Konfiguracja polaczenia");
+        userDialog.setTitle("Konfiguracja polaczenia");
+        userDialog.setHeaderText("Krok 2/3: Nazwa użytkownika");
         result = userDialog.showAndWait();
         if (result.isPresent()) {
             user = result.get();
         }
         else {
-            System.exit(0);
+            logger.log(Level.CONFIG, "Nie wprowadzono nazwy uzytkownika BD");
+            System.exit(1);
         }
+        
+        TextInputDialog passwordDialog = new TextInputDialog("Konfiguracja polaczenia");
+        passwordDialog.setTitle("Konfiguracja polaczenia");
+        passwordDialog.setHeaderText("Krok 3/3: Haslo");
         result = passwordDialog.showAndWait();
         if (result.isPresent()) {
             password = result.get();
         }
         else {
-            System.exit(0);
+            logger.log(Level.CONFIG, "Nie wprowadzono hasla");
+            System.exit(1);
         }
     
         Properties defSettings = new Properties();
@@ -78,9 +85,11 @@ public final class Configurator {
             FileOutputStream out = new FileOutputStream("program.properties");
             currentSettings.store(out, "Ustawienia programu");
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE,"Nie znaleziono pliku konfiguracyjnego (prawdopodobnie usuniety recznie)");
+            System.exit(1);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE,"Nieudana proba zapisania pliku konfiguracyjnego");
+            System.exit(1);
         }
     }
     public static void importDefaultSettings(){
@@ -93,9 +102,11 @@ public final class Configurator {
             defaultSettings.load(in);
             currentSettings = (Properties) defaultSettings.clone();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE,"Nie znaleziono pliku konfiguracyjnego (prawdopodobnie usuniety recznie)");
+            System.exit(1);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE,"Nieudana proba zaladowania pliku konfiguracyjnego");
+            System.exit(1);
         }
     }
     
@@ -105,9 +116,9 @@ public final class Configurator {
             FileOutputStream out = new FileOutputStream("program.properties");
             currentSettings.store(out, "Ustawienia programu");
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            logger.log(Level.WARNING,"Nie znaleziono pliku konfiguracyjnego przy probie zapisu");
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.WARNING,"Nieudana proba zapisu pliku konfiguracyjnego");
         }
     }
     public void addProperty(String key, String value){
