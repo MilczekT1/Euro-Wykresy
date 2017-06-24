6b138633a18e96e2dec3219b623929a4afc00625
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-06-24 13:13:45 +0200

    sample.Main -> sample.General, final class

--- src/main/java/sample/General/ThreadPool.java
+++ src/main/java/sample/General/ThreadPool.java
@@ -1,9 +1,9 @@
-package sample.Main;
+package sample.General;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class ThreadPool {
+public final class ThreadPool {
     
     private static ExecutorService Instance = Executors.newCachedThreadPool();
     public static ExecutorService getInstance(){
