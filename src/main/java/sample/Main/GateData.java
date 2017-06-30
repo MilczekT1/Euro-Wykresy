de7f5ceb6682fae710cc6d771be27da9333b91f5
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-06-30 21:26:35 +0200

    gateId is now final; getters using lombok

--- src/main/java/sample/Main/GateData.java
+++ src/main/java/sample/Main/GateData.java
@@ -1,21 +1,15 @@
 package sample.Main;
 
+import lombok.Getter;
 import javax.sql.rowset.CachedRowSet;
 
-public class GateData {
-    private String gateId;
+@Getter
+class GateData {
+    private final String gateId;
     private CachedRowSet values;
     
     public GateData(String gateId, CachedRowSet values) {
         this.gateId = gateId;
         this.values = values;
-    }
-    
-    public CachedRowSet getValues() {
-        return values;
-    }
-    
-    public String getGateId() {
-        return gateId;
     }
 }
