a92a9b0c1dc83a499d890ceba54fe1b7c3ccaadb
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-07-04 10:16:48 +0200

    KONRAD_BRAMKI and KONRAD_GRUPY tables are now in relation many-to-one

--- src/main/resources/Create_Groups.sql
+++ src/main/resources/Create_Groups.sql
@@ -7,10 +7,9 @@ CREATE TABLE KONRAD_GRUPY(
     Id_Grupy int IDENTITY(1,1) PRIMARY KEY,
     Nazwa varchar(50) NOT NULL UNIQUE,
 );
-
 USE wizualizacja2;/**/
 CREATE TABLE KONRAD_BRAMKI(
-    Id_Grupy int NOT NULL,
+    Id_Grupy int NOT NULL REFERENCES KONRAD_GRUPY(Id_Grupy) ON DELETE CASCADE,
     GateId int NOT NULL,
     PRIMARY KEY (Id_Grupy,GateId)
 );
