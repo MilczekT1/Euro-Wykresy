e87e8dbc6111b38138ce1700f9318eea96bca178
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-07-07 00:42:43 +0200

    flexible amount of threads

--- src/main/java/sample/General/ThreadPool.java
+++ src/main/java/sample/General/ThreadPool.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 public final class ThreadPool {
     
-    private static ExecutorService Instance = Executors.newFixedThreadPool(8);
+    private static ExecutorService Instance = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     public static ExecutorService getInstance(){
         return Instance;
     }
