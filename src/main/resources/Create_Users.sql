169607fee6f2133bcec366496a5f58a81fed8294
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-07-18 12:33:04 +0200

    simple refactoring

--- src/main/resources/Create_Users.sql
+++ src/main/resources/Create_Users.sql
@@ -2,8 +2,8 @@
  * DO_NOT_DELETE_COMMENTS
  */
 
-USE wizualizacja2;/**/
-CREATE TABLE KONRAD_UZYTKOWNICY(
+USE wizualizacja;/**/
+CREATE TABLE EW_Uzytkownicy(
     Id_Uzytkownika int IDENTITY(1,1) PRIMARY KEY,
     Login varchar(40) UNIQUE,
     Haslo varchar(255),
