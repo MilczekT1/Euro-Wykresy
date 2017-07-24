6026f95c0dfa5b183ac2655c22597d111d0234cc
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-07-24 09:45:49 +0200

    simple refactoring

--- src/main/java/pl/konradboniecki/main/Chart.java
+++ src/main/java/pl/konradboniecki/main/Chart.java
@@ -4,6 +4,7 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.time.*;
 import org.jfree.data.xy.XYDataset;
+import pl.konradboniecki.structures.GateData;
 import pl.konradboniecki.general.Configurator;
 
 import javax.sql.rowset.CachedRowSet;
@@ -30,14 +31,15 @@ class Chart {
         return ChartFactory.createTimeSeriesChart(title,"Czas", measureType,dataset,true,true,false);
     }
     
-    static CachedRowSet dbImportGateValues(String gateId, long start, long end) throws SQLException{
+    static CachedRowSet dbImportGateValues(String gateId, long start, long end) throws SQLException {
+        //TODO: poprawic  skrypt
         String sql = "use e6_VfiTag;\n" +
                              "SELECT time, value FROM VfiTagNumHistory\n" +
                              "WHERE gateId = ? AND time BETWEEN ? AND ?;";
         CachedRowSet crs = new com.sun.rowset.CachedRowSetImpl();
-        crs.setUrl("jdbc:sqlserver://" + Configurator.getCurrentSettings().getProperty("Server-Adress"));
-        crs.setUsername(Configurator.getCurrentSettings().getProperty("User"));
-        crs.setPassword(Configurator.getCurrentSettings().getProperty("Password"));
+        crs.setUrl("jdbc:sqlserver://" + Configurator.getCurrentSettings().getProperty("Adress-Treblinka-1"));
+        crs.setUsername(Configurator.getCurrentSettings().getProperty("User-Treblinka-1"));
+        crs.setPassword(Configurator.getCurrentSettings().getProperty("Password-Treblinka-1"));
         crs.setCommand(sql);
         crs.setString(1,gateId);
         crs.setLong(2,start);
