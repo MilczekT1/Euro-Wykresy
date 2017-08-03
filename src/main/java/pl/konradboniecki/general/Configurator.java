763d36db77a08242d1fa70bbee475426ae1ec5cf
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-08-03 17:19:52 +0200

    redesigned configurator

--- src/main/java/pl/konradboniecki/general/Configurator.java
+++ src/main/java/pl/konradboniecki/general/Configurator.java
@@ -3,6 +3,8 @@ package pl.konradboniecki.general;
 import lombok.Cleanup;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -24,51 +26,30 @@ public final class Configurator {
         }
     }
     private static void setDefaultSettings(){
+        ArrayList<String> adresses = new ArrayList<>(3);
+        ArrayList<String> users = new ArrayList<>(3);
+        ArrayList<String> passwords = new ArrayList<>(3);
         String firstAdress = null;
         String secondAdress = null;
         String user = null;
         String password = null;
-        /*String paterekAdress = null;
-        String paterekUser = null;
-        String paterekPassword = null;*/
+        String localisation = null;
         Optional<String> result;
-        
-        /*
-         * Paterek
-        result = Utils.getStringFromDialog("Konfiguracja polaczenia", "Konfiguracja polaczenia","Krok 1/3: IP + port do paterka");
-        if (result.isPresent())
-            paterekAdress = result.get();
-        else {
-            logger.log(Level.CONFIG, "Nie wprowadzono adresu ip i/lub portu");
-            System.exit(1);
-        }
-        result = Utils.getStringFromDialog("Konfiguracja polaczenia", "Konfiguracja polaczenia","Krok 2/3: Nazwa użytkownika do paterka");
-        if (result.isPresent()) {
-            paterekUser = result.get();
-        }
-        else {
-            logger.log(Level.CONFIG, "Nie wprowadzono nazwy uzytkownika BD");
-            System.exit(1);
-        }
     
-        result = Utils.getStringFromDialog("Konfiguracja polaczenia", "Konfiguracja polaczenia", "Krok 3/3: Haslo do paterka");
+        result = Utils.getStringFromDialog("LOKALIZACJA PATEREK/TRZEBINIA", "LOKALIZACJA PATEREK/TRZEBINIA","LOKALIZACJA PATEREK/TRZEBINIA");
         if (result.isPresent()) {
-            paterekPassword = result.get();
-        }
-        else {
-            logger.log(Level.CONFIG, "Nie wprowadzono hasla");
+            localisation = result.get();
+            localisation = localisation.toLowerCase();
+        } else {
+            logger.log(Level.CONFIG, "Nie wprowadzono lokalizacji");
             System.exit(1);
         }
-        */
+        
+        setFirstTreblinka(adresses,users,passwords);
+        setSecondTreblinka(adresses,users,passwords);
+        setPaterek(adresses, users, passwords);
         
         
-        result = Utils.getStringFromDialog("Konfiguracja polaczenia", "Konfiguracja polaczenia","Krok 1/4: IP + port");
-        if (result.isPresent())
-            firstAdress = result.get();
-        else {
-            logger.log(Level.CONFIG, "Nie wprowadzono adresu ip i/lub portu");
-            System.exit(1);
-        }
         
         result = Utils.getStringFromDialog("Konfiguracja polaczenia", "Konfiguracja polaczenia", "Krok 2/4: drugie IP + port");
         if (result.isPresent()) {
@@ -78,35 +59,21 @@ public final class Configurator {
             logger.log(Level.CONFIG, "Nie wprowadzono adresu ip i/lub portu");
             System.exit(1);
         }
-    
-        result = Utils.getStringFromDialog("Konfiguracja polaczenia", "Konfiguracja polaczenia","Krok 3/4: Nazwa użytkownika");
-        if (result.isPresent()) {
-            user = result.get();
-        }
-        else {
-            logger.log(Level.CONFIG, "Nie wprowadzono nazwy uzytkownika BD");
-            System.exit(1);
-        }
-    
-        result = Utils.getStringFromDialog("Konfiguracja polaczenia", "Konfiguracja polaczenia", "Krok 4/4: Haslo");
-        if (result.isPresent()) {
-            password = result.get();
-        }
-        else {
-            logger.log(Level.CONFIG, "Nie wprowadzono hasla");
-            System.exit(1);
-        }
         
         Properties defSettings = new Properties();
-        defSettings.setProperty("User-Treblinka-1", user);
-        defSettings.setProperty("Password-Treblinka-1", password);
-        defSettings.setProperty("Adress-Treblinka-1", firstAdress);
-        defSettings.setProperty("User-Treblinka-2", user);
-        defSettings.setProperty("Password-Treblinka-2", password);
-        defSettings.setProperty("Adress-Treblinka-2", secondAdress);
-        /*defSettings.setProperty("User-Paterek", paterekUser);
-        defSettings.setProperty("Password-Paterek", paterekPassword);
-        defSettings.setProperty("Paterek-Adress", paterekAdress);*/
+        defSettings.setProperty("localisation", localisation);
+        
+        defSettings.setProperty("User-Treblinka-1", users.remove(0));
+        defSettings.setProperty("Password-Treblinka-1", passwords.remove(0));
+        defSettings.setProperty("Adress-Treblinka-1", adresses.remove(0));
+        
+        defSettings.setProperty("User-Treblinka-2", users.remove(0));
+        defSettings.setProperty("Password-Treblinka-2", passwords.remove(0));
+        defSettings.setProperty("Adress-Treblinka-2", adresses.remove(0));
+        
+        defSettings.setProperty("User-Paterek", users.remove(0));
+        defSettings.setProperty("Password-Paterek", passwords.remove(0));
+        defSettings.setProperty("Paterek-Adress", adresses.remove(0));
         
         try {
             currentSettings =  (Properties) defSettings.clone();
@@ -154,5 +121,93 @@ public final class Configurator {
     
     public static String getCurrentProperty(String key){
         return Configurator.getCurrentSettings().getProperty(key);
+    }
+    
+    private static void setPaterek(List<String> adresses, List<String> users, List<String> passwords){
+        Optional<String> result;
+        
+        result = Utils.getStringFromDialog("Konfiguracja polaczenia", "Konfiguracja polaczenia","Krok 1/3: IP + port do paterka");
+        if (result.isPresent())
+            adresses.add(result.get());
+        else {
+            logger.log(Level.CONFIG, "Nie wprowadzono adresu ip i/lub portu");
+            System.exit(1);
+        }
+        
+        result = Utils.getStringFromDialog("Konfiguracja polaczenia", "Konfiguracja polaczenia","Krok 2/3: Nazwa użytkownika do paterka");
+        if (result.isPresent()) {
+            users.add(result.get());
+        }
+        else {
+            logger.log(Level.CONFIG, "Nie wprowadzono nazwy uzytkownika BD");
+            System.exit(1);
+        }
+    
+        result = Utils.getStringFromDialog("Konfiguracja polaczenia", "Konfiguracja polaczenia", "Krok 3/3: Haslo do paterka");
+        if (result.isPresent()) {
+            passwords.add(result.get());
+        }
+        else {
+            logger.log(Level.CONFIG, "Nie wprowadzono hasla");
+            System.exit(1);
+        }
+    }
+    private static void setFirstTreblinka(List<String> adresses, List<String> users, List<String> passwords){
+        Optional<String> result;
+        
+        result = Utils.getStringFromDialog("Glowny serwer", "Konfiguracja polaczenia","Krok 1/3: IP + port");
+        if (result.isPresent())
+            adresses.add(result.get());
+        else {
+            logger.log(Level.CONFIG, "Nie wprowadzono adresu ip i/lub portu");
+            System.exit(1);
+        }
+    
+        result = Utils.getStringFromDialog("Glowny serwer", "Konfiguracja polaczenia","Krok 2/3: Nazwa użytkownika");
+        if (result.isPresent()) {
+            users.add(result.get());
+        }
+        else {
+            logger.log(Level.CONFIG, "Nie wprowadzono nazwy uzytkownika BD");
+            System.exit(1);
+        }
+    
+        result = Utils.getStringFromDialog("Glowny serwer", "Konfiguracja polaczenia", "Krok 3/3: Haslo");
+        if (result.isPresent()) {
+            passwords.add(result.get());
+        }
+        else {
+            logger.log(Level.CONFIG, "Nie wprowadzono hasla");
+            System.exit(1);
+        }
+    }
+    private static void setSecondTreblinka(List<String> adresses, List<String> users, List<String> passwords){
+        Optional<String> result;
+        
+        result = Utils.getStringFromDialog("Drugi serwer", "Konfiguracja polaczenia","Krok 1/3: IP + port");
+        if (result.isPresent())
+            adresses.add(result.get());
+        else {
+            logger.log(Level.CONFIG, "Nie wprowadzono adresu ip i/lub portu");
+            System.exit(1);
+        }
+        
+        result = Utils.getStringFromDialog("Drugi serwer", "Konfiguracja polaczenia","Krok 2/3: Nazwa użytkownika");
+        if (result.isPresent()) {
+            users.add(result.get());
+        }
+        else {
+            logger.log(Level.CONFIG, "Nie wprowadzono nazwy uzytkownika BD");
+            System.exit(1);
+        }
+        
+        result = Utils.getStringFromDialog("Drugi serwer", "Konfiguracja polaczenia", "Krok 3/3: Haslo");
+        if (result.isPresent()) {
+            passwords.add(result.get());
+        }
+        else {
+            logger.log(Level.CONFIG, "Nie wprowadzono hasla");
+            System.exit(1);
+        }
     }
 }
