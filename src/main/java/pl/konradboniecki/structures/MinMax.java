1da8c25ccdf4d56314d478650a21cd64c655978c
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-08-03 17:34:36 +0200

    ..

--- src/main/java/pl/konradboniecki/structures/MinMax.java
+++ src/main/java/pl/konradboniecki/structures/MinMax.java
@@ -11,4 +11,15 @@ public class MinMax {
         this.min = min;
         this.max = max;
     }
+    public MinMax(MinMax first, MinMax second){
+        if (first.getMax() >= second.getMax())
+            this.max = first.getMax();
+        else
+            this.max = second.getMax();
+        
+        if (first.getMin() <= second.getMin())
+            this.min = first.getMin();
+        else
+            this.min = second.getMin();
+    }
 }
